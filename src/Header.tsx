05e867a2f2b59e4efb66ce48024626be8d77868b
LDK <lemondropkid@gmail.com>
2023-03-31 00:33:27 -0700

    Some theme updates

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,16 +1,36 @@
 // Header.tsx
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar, Typography, useTheme } from '@mui/material';
 import Sticky from 'react-stickynode';
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import { ThemeName } from './theme';
 
-const Header: React.FC = () => {
+type HeaderProps = {
+  handleThemeChange: (arg:ThemeName) => void;
+  theme: ThemeName;
+}
+
+const Header: React.FC<HeaderProps> = ({ theme:activeTheme, handleThemeChange }) => {
+  const theme = useTheme();
+  
   return (
     <Sticky innerZ={2}>
-      <AppBar>
+      <AppBar sx={{ backgroundColor: theme.palette.primary.main }}>
         <Toolbar>
-          <Typography variant="h6" component="div" py={0}>
-            My Application
-          </Typography>
+          <FormControl>
+            <InputLabel id="theme-select-label">{ Boolean(activeTheme) ? '' : 'Theme' }</InputLabel>
+            <Select
+              variant="filled"
+              sx={{ background: theme.palette.background.paper, color: theme.palette.text.primary }}
+              labelId="theme-select-label"
+              id="theme-select"
+              value={activeTheme}
+              onChange={(e) => handleThemeChange(e.target.value as ThemeName)}
+            >
+              <MenuItem value={'light'}>Light</MenuItem>
+              <MenuItem value={'dark'}>Dark</MenuItem>
+            </Select>
+          </FormControl>
         </Toolbar>
       </AppBar>
     </Sticky>
--- src/theme.tsx
+++ src/theme.tsx
@@ -1,58 +1,18 @@
 // theme.tsx
 import { createTheme } from '@mui/material/styles';
 
-const lightTheme = createTheme({
-  palette: {
-    mode: 'light',
-    primary: {
-      // Calm blue color, chosen for a professional appearance and readability.
-      main: '#5c6bc0',
-    },
-    secondary: {
-      // Soft orange color, chosen for a subtle contrast to the primary color, used for accents and highlights.
-      main: '#ffab91',
-    },
-    background: {
-      // Light gray color, chosen for a neutral and gentle background that's easy on the eyes.
-      default: '#f5f5f5',
-      // White color, used for paper elements and containers for better visual separation.
-      paper: '#ffffff',
-    },
-    text: {
-      // Dark gray color, chosen for readability against the light background.
-      primary: '#424242',
-      // Medium gray color, used for less important text elements, providing a visual hierarchy.
-      secondary: '#757575',
-    },
-    success: {
-      // Green color, typically associated with success messages or positive actions.
-      main: '#4caf50',
-    },
-    warning: {
-      // Yellow color, typically associated with warning messages or cautionary actions.
-      main: '#ff9800',
-    },
-    error: {
-      // Red color, typically associated with error messages or negative actions.
-      main: '#f44336',
-    },
-    info: {
-      // Blue color, typically associated with informational messages or neutral actions.
-      main: '#2196f3',
-    },
-  },
-});
+export type ThemeName = 'light' | 'dark';
 
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
     primary: {
-      // Warm orange color, chosen for a vivid and dynamic appearance, providing good contrast with the dark background.
-      main: '#ff9800',
+      // Calm blue color, chosen for a professional appearance and readability.
+      main: '#4f5fd5',
     },
     secondary: {
-      // Bright blue color, chosen for a striking contrast to the primary color, used for accents and highlights.
-      main: '#29b6f6',
+      // Calm blue color, chosen for a professional appearance and readability.
+      main: '#4f5fd5',
     },
     background: {
       // Dark gray color, chosen for a modern and focused appearance that reduces eye strain in low-light environments.
@@ -67,12 +27,54 @@ const darkTheme = createTheme({
       secondary: '#e0e0e0',
     },
     success: {
-      // Green color, adjusted for dark mode readability.
-      main: '#81c784',
+      // Green color, typically associated with success messages or positive actions.
+      main: '#4caf50',
     },
     warning: {
       // Yellow color, adjusted for dark mode readability.
       main: '#ffb74d',
+    },
+    error: {
+      // Red color, typically associated with error messages or negative actions.
+      main: '#f44336',
+    },
+    info: {
+      // Blue color, typically associated with informational messages or neutral actions.
+      main: '#2196f3',
+    },
+  },
+});
+
+const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      // Calm blue color, chosen for a professional appearance and readability.
+      main: '#4f8fdd',
+    },
+    secondary: {
+      // Bright blue color, chosen for a striking contrast to the primary color, used for accents and highlights.
+      main: '#77c0ea',
+    },
+    background: {
+      // Light gray color, chosen for a neutral and gentle background that's easy on the eyes.
+      default: '#f5f5f5',
+      // White color, used for paper elements and containers for better visual separation.
+      paper: '#ffffff',
+    },
+    text: {
+      // Dark gray color, chosen for readability against the light background.
+      primary: '#424242',
+      // Medium gray color, used for less important text elements, providing a visual hierarchy.
+      secondary: '#757575',
+    },
+    success: {
+      // Green color, adjusted for dark mode readability.
+      main: '#81c784',
+    },
+    warning: {
+      // Yellow color, typically associated with warning messages or cautionary actions.
+      main: '#ff9800',
     },
     error: {
       // Red color, adjusted for dark mode readability.
