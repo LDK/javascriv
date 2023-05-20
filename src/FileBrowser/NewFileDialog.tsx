c2a4754d6abdee82b5d4ec5c238dd8ec5478401e
LDK <lemondropkid@gmail.com>
2023-05-19 21:25:33 -0700

    Validation (with tooltip over disabled Create button) for New File dialog, checking for valid file name and lack of same-name siblings

--- src/FileBrowser/NewFileDialog.tsx
+++ src/FileBrowser/NewFileDialog.tsx
@@ -1,7 +1,7 @@
-import { Dialog, DialogContent, DialogContentText, TextField, FormControl, InputLabel, Select, MenuItem, DialogActions, Button } from "@mui/material";
+import { Dialog, DialogContent, DialogContentText, TextField, FormControl, InputLabel, Select, MenuItem, DialogActions, Button, Tooltip } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addItem, BrowserItem, selectFiles } from "../redux/filesSlice";
+import { addItem, BrowserItem, findItemByPath, selectFiles } from "../redux/filesSlice";
 import { FileType, findParentFolder, ROOTFOLDER, SubType } from "./FileBrowser";
 import { getFolders, SetOpenFunction } from "./useBrowserDialog";
 
@@ -24,6 +24,10 @@ const NewFileDialog = ({
   const itemName = fileType === 'file' ? subType as string : 'folder';
   const [newItemName, setNewItemName] = useState('');
   const [parentFolder, setParentFolder] = useState<string | null>(openFolder);
+  const [siblings, setSiblings] = useState<BrowserItem[]>([]);
+  const [hasTwin, setHasTwin] = useState(false);
+  const [disabledReason, setDisabledReason] = useState<string>('');
+  const submitDisabled = !newItemName || hasTwin;
 
   const dispatch = useDispatch();
   
@@ -43,6 +47,18 @@ const NewFileDialog = ({
   };
 
   useEffect(() => {
+    if (submitDisabled) {
+      if (!newItemName) {
+        setDisabledReason('Please enter a name');
+      } else if (hasTwin) {
+        setDisabledReason(`A ${itemName} with this name already exists`);
+      }
+    } else {
+      setDisabledReason('');
+    }
+  }, [hasTwin, itemName, newItemName, submitDisabled]);
+
+  useEffect(() => {
     const newFolder = findParentFolder(openFilePath?.split('/') || []);
     if (newFolder !== openFolder) {
       setOpenFolder(newFolder);
@@ -50,32 +66,50 @@ const NewFileDialog = ({
         setParentFolder(newFolder);
       }
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [openFilePath]);
+  }, [openFilePath, open, openFolder, setOpenFolder]);
 
   useEffect(() => {
     if (!open) {
       setParentFolder(openFolder);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [openFolder]);
-  
+  }, [open, openFolder, setParentFolder]);
+
+  useEffect(() => {
+    const parentItem = findItemByPath(items, parentFolder?.split('/') || []);
+
+    if (parentItem) {
+      setSiblings(parentItem.children || []);
+    }
+  }, [parentFolder, items]);
+
+  useEffect(() => {
+    if (siblings.length) {  
+      const existing = siblings.find(s => s.name === newItemName);
+      setHasTwin(Boolean(existing));
+    } else {
+      setHasTwin(false);
+    }
+  }, [newItemName, siblings]);
+
   return (
     <Dialog open={open} onClose={onClose}>
       <DialogContent>
-        <DialogContentText>
+        <DialogContentText sx={{ mb: 2 }}>
           Please enter the name of the new {itemName} and select the parent folder.
         </DialogContentText>
         <TextField
+          sx={{ my : 1 }}
+          color="info"
           autoFocus
           margin="dense"
           label="Name"
           type="text"
           fullWidth
+          required={true}
           value={newItemName}
           onChange={(e) => setNewItemName(e.target.value)}
         />
-        <FormControl fullWidth margin="dense">
+        <FormControl fullWidth margin="dense" color="info">
           <InputLabel id="parent-folder-label">Parent Folder</InputLabel>
           <Select
             labelId="parent-folder-label"
@@ -99,9 +133,14 @@ const NewFileDialog = ({
           </Select>
         </FormControl>
       </DialogContent>
-      <DialogActions>
-        <Button onClick={onClose}>Cancel</Button>
-        <Button onClick={handleCreateNewFile}>Create</Button>
+      <DialogActions sx={{ px: 3, pb: 3 }}>
+        <Button onClick={onClose} variant="outlined" color="warning" sx={{ mr: 1 }}>Cancel</Button>
+
+        <Tooltip title={disabledReason || ''}>
+          <span>
+            <Button variant="contained" color="success" disabled={submitDisabled} onClick={handleCreateNewFile} type="submit">Create</Button>
+          </span>
+        </Tooltip>
       </DialogActions>
     </Dialog>
   );
